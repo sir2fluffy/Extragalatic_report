6870c0623fb8cbaea7164be73707b0a01567b690
Charles GH <charlesvyelland@rocketmail.com>
2021-04-09 18:34:17 +0100

    Fixed load last

--- Main.py
+++ Main.py
@@ -26,17 +26,11 @@ import matplotlib.pyplot as pl
 #cz = H_o d
 #cz/h_o = d
 
-def distance(z):
-    c = const.c.to('km/s')
-    H_0 = cosmo.H(0)  #km / Mpc s
-    d = (c*z/H_0).value
-    return d # usings of Mpc
-
-
 
 SDSS = CSV('data3 data with a vengence')
 SDSS_data = SDSS.read_all()
 # -1 because of column titles
+print(SDSS.row_count_data)
 calculated = np.zeros((SDSS.row_count_data,3))#add area
 print('read data')
 for index,row in enumerate(SDSS_data):
@@ -63,7 +57,7 @@ for index,row in enumerate(SDSS_data):
 
 
 pl.scatter(SDSS_data[:,0],calculated[:,1],s=.01)
-pl.plot([-.1,.45,.45],[-22,-22,-27],color = 'red')
+pl.plot([-.1,.4,.4],[-22.6,-22.6,-27],color = 'red')
 pl.plot(.35,-21,'+',color = 'yellow')
 pl.gca().invert_yaxis()
 pl.ylim(-10,-27)
@@ -75,7 +69,7 @@ red = 0
 green = 0
 selected_galaxies = []
 for index in range(0,len(SDSS_data[:,0])):
-    if SDSS_data[index,0] <= .45 and calculated[index,1] <= -22:     #red
+    if SDSS_data[index,0] <= .4 and calculated[index,1] <= -22.6:     #red
         selected_galaxies.append(index)
         red = red + 1
 
@@ -91,21 +85,17 @@ for index,galaxy_index in enumerate(selected_galaxies):
     if z < min_z:
         min_z = z
     
-    z, zErr, cModelMag_r, cModelMagErr_r, cModelMag_u, cModelMagErr_u, petroR90_r, petroR90Err_r, modelMag_r, modelMag_u = SDSS_data[galaxy_index,:]
+    z, zErr, petroMag_r, petroMagErr_r, cModelMag_u, cModelMagErr_u, petroRad_r, petroRadErr_r, modelMag_r, modelMag_u = SDSS_data[galaxy_index,:]
     
        #https://www.sdss.org/dr12/algorithms/magnitudes/#:~:text=In%20SDSS%2DIII%2C%20we%20express,are%20a%20convenient%20linear%20unit.&text=To%20relate%20these%20quantities%20to,%5D%20%E2%80%93%202.5%20log10%20f%20.
        
-    flux = 10**((22.5-cModelMag_r)/25)   
+    flux = 10**((22.5-petroMag_r)/25)   
     mu = flux/(np.pi * (petroRad_r**2))
 
         
     fit_data[index,:] = z, mu
 
 
-pl.scatter(fit_data[:,0],fit_data[:,1],s=0.1,color = 'red')
-pl.xlabel('Z')
-pl.ylabel('mu')
-pl.show()
 
 #convert from flux to SB
 
